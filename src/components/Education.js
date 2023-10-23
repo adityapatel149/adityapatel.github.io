b23c32da52e0705b6fab40be0e2e14ac5878ee03
AdityaPatel149 <55062974+adityapatel149@users.noreply.github.com>
2023-10-23 11:33:59 +0000

    dark mode styling

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,72 +1,140 @@
-export default function WorkExperience(){
-    return (
-        <section className="education section-container">
-            <h2>Education</h2>
-            <div className="content-container">
-                <div className="timeline-item">
-                    <div className="timeline-line">
-                        <div className="dot">
-                            <img src={'/svg/dot.svg'}/>
-                        </div>
-                        <div className="divider">
-                            <div className="line" />
-                        </div>
-                        
-                    </div>
-                    <div className="timeline-item-content">
-                        <div className="date-time">
-                            <div className="badge"><p>Always</p></div>
-                            <div className="location">
-                                <img src={'/svg/location.svg'}/>
-                                <p>Everywhere</p>
-                            </div>
-                        </div>
-                        <div className="item-description">
-                            <div className="item-header">
-                                <img src={"/images/meta.png"}/>
-                                <div className="item-name">
-                                    <p className="subtitle">Self-learner</p>
-                                    <p className="title">From each job, class, article, co-worker, book, or life experiences</p>
-                                </div>
-                            </div>
-                            <ul>
-                                <li><p>This is how I learnt most of the things I know and how I improve it. At the moment, I am learning ReactJS, Redux, Product Design, and Brand Design.</p></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="timeline-item">
-                    <div className="timeline-line">
-                        <div className="dot">
-                            <img src={'/svg/dot.svg'}/>
-                        </div>                        
-                    </div>
-                    <div className="timeline-item-content">
-                        <div className="date-time">
-                            <p>2019-2023</p>
-                            <div className="location">
-                                <img src={'/svg/location.svg'}/>
-                                <p>Vadodara, India</p>
-                            </div>
-                        </div>
-                        <div className="item-description">
-                            <div className="item-header">
-                                <img src={"/images/meta.png"}/>
-                                <div className="item-name">
-                                    <p className="subtitle">COMPUTER SCIENCE & ENGINEERING, BACHELOR OF ENGINEERING (B.E.)</p>
-                                    <p className="title">Babaria Institute of Technology & Science</p>
-                                </div>
-                            </div>
-                            <ul>
-                                <li><p>I studied my bachelor degree in this college. This is where I learnt Engineering subjects, Programming, Web Development, Database Management, Computer Architecture, and Networking. CGPA:9.4/10</p></li>                            
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
+export default function WorkExperience() {
+  return (
+    <section className="education section-container">
+      <h2>Education</h2>
+      <div className="content-container">
+        <div className="timeline-item">
+          <div className="timeline-line">
+            <div className="dot">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="misc/dot_04_l">
+                  <path
+                    id="Icon"
+                    d="M15 21.25C11.5482 21.25 8.75 18.4518 8.75 15C8.75 11.5482 11.5482 8.75 15 8.75C18.4518 8.75 21.25 11.5482 21.25 15C21.25 18.4518 18.4518 21.25 15 21.25Z"
+                  />
+                </g>
+              </svg>
             </div>
-        </section>
-    )
+            <div className="divider">
+              <div className="line" />
+            </div>
+          </div>
+          <div className="timeline-item-content">
+            <div className="date-time">
+              <div className="badge">
+                <p>Always</p>
+              </div>
+              <div className="location">
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="Icon"
+                    d="M16.0003 27.9971C14.3162 26.5606 12.7552 24.9858 11.3337 23.2891C9.20033 20.7411 6.66699 16.9465 6.66699 13.3305C6.6651 9.55387 8.93932 6.14832 12.4284 4.703C15.9175 3.25768 19.9338 4.05746 22.603 6.72914C24.3583 8.47661 25.3415 10.8537 25.3337 13.3305C25.3337 16.9465 22.8003 20.7411 20.667 23.2891C19.2454 24.9858 17.6844 26.5606 16.0003 27.9971ZM16.0003 9.33047C14.5713 9.33047 13.2508 10.0929 12.5362 11.3305C11.8217 12.5681 11.8217 14.0929 12.5362 15.3305C13.2508 16.5681 14.5713 17.3305 16.0003 17.3305C18.2095 17.3305 20.0003 15.5396 20.0003 13.3305C20.0003 11.1213 18.2095 9.33047 16.0003 9.33047Z"
+                  />
+                </svg>
+                <p>Everywhere</p>
+              </div>
+            </div>
+            <div className="item-description">
+              <div className="item-header">
+                <img src={"/images/meta.png"} />
+                <div className="item-name">
+                  <p className="subtitle">Self-learner</p>
+                  <p className="title">
+                    From each job, class, article, co-worker, book, or life
+                    experiences
+                  </p>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <p>
+                    This is how I learnt most of the things I know and how I
+                    improve it. At the moment, I am learning ReactJS, Redux,
+                    Product Design, and Brand Design.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="timeline-item">
+          <div className="timeline-line">
+            <div className="dot">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="misc/dot_04_l">
+                  <path
+                    id="Icon"
+                    d="M15 21.25C11.5482 21.25 8.75 18.4518 8.75 15C8.75 11.5482 11.5482 8.75 15 8.75C18.4518 8.75 21.25 11.5482 21.25 15C21.25 18.4518 18.4518 21.25 15 21.25Z"
+                  />
+                </g>
+              </svg>
+            </div>
+          </div>
+          <div className="timeline-item-content">
+            <div className="date-time">
+              <p>2019-2023</p>
+              <div className="location">
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="Icon"
+                    d="M16.0003 27.9971C14.3162 26.5606 12.7552 24.9858 11.3337 23.2891C9.20033 20.7411 6.66699 16.9465 6.66699 13.3305C6.6651 9.55387 8.93932 6.14832 12.4284 4.703C15.9175 3.25768 19.9338 4.05746 22.603 6.72914C24.3583 8.47661 25.3415 10.8537 25.3337 13.3305C25.3337 16.9465 22.8003 20.7411 20.667 23.2891C19.2454 24.9858 17.6844 26.5606 16.0003 27.9971ZM16.0003 9.33047C14.5713 9.33047 13.2508 10.0929 12.5362 11.3305C11.8217 12.5681 11.8217 14.0929 12.5362 15.3305C13.2508 16.5681 14.5713 17.3305 16.0003 17.3305C18.2095 17.3305 20.0003 15.5396 20.0003 13.3305C20.0003 11.1213 18.2095 9.33047 16.0003 9.33047Z"
+                  />
+                </svg>
+                <p>Vadodara, India</p>
+              </div>
+            </div>
+            <div className="item-description">
+              <div className="item-header">
+                <img src={"/images/meta.png"} />
+                <div className="item-name">
+                  <p className="subtitle">
+                    COMPUTER SCIENCE & ENGINEERING, BACHELOR OF ENGINEERING
+                    (B.E.)
+                  </p>
+                  <p className="title">
+                    Babaria Institute of Technology & Science
+                  </p>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <p>
+                    I studied my bachelor degree in this college. This is where
+                    I learnt Engineering subjects, Programming, Web Development,
+                    Database Management, Computer Architecture, and Networking.
+                    CGPA:9.4/10
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,111 +1,218 @@
-export default function WorkExperience(){
-    return (
-        <section className="work-experience section-container">
-            <h2>Work Experience</h2>
-            <div className="content-container">
-                <div className="timeline-item">
-                    <div className="timeline-line">
-                        <div className="dot">
-                            <img src={'/svg/dot.svg'}/>
-                        </div>
-                        <div className="divider">
-                            <div className="line" />
-                        </div>
-                        
-                    </div>
-                    <div className="timeline-item-content">
-                        <div className="date-time">
-                            <div className="badge"><p>Present</p></div>
-                            <div className="location">
-                                <img src={'/svg/location.svg'}/>
-                                <p>Vadodara, India</p>
-                            </div>
-                        </div>
-                        <div className="item-description">
-                            <div className="item-header">
-                                <img src={"/images/meta.png"}/>
-                                <div className="item-name">
-                                    <p className="subtitle">UI/UX Engineer</p>
-                                    <p className="title">Electrum IT Solutions</p>
-                                </div>
-                            </div>
-                            <ul>
-                                <li><p>Conducted user research through surveys and interviews, informing the design process.</p></li>
-                                <li><p>Produced wireframes, interactive prototypes, and high fidelity UI designs with tools like Figma, Adobe Illustrator, and Adobe After Effects, optimizing design-to-development flow.</p></li>
-                                <li><p>Collaborated with cross-functional teams to translate user requirements into effective design solutions.</p></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="timeline-item">
-                    <div className="timeline-line">
-                        <div className="dot">
-                            <img src={'/svg/dot.svg'}/>
-                        </div>
-                        <div className="divider">
-                            <div className="line" />
-                        </div>
-                        
-                    </div>
-                    <div className="timeline-item-content">
-                        <div className="date-time">
-                            <p>Feb 2023 - May 2023</p>
-                            <div className="location">
-                                <img src={'/svg/location.svg'}/>
-                                <p>Vadodara, India</p>
-                            </div>
-                        </div>
-                        <div className="item-description">
-                            <div className="item-header">
-                                <img src={"/images/meta.png"}/>
-                                <div className="item-name">
-                                    <p className="subtitle">UI/UX ENGINEER INTERN</p>
-                                    <p className="title">Electrum IT Solutions</p>
-                                </div>
-                            </div>
-                            <ul>
-                                <li><p>During the internship, contributed to wireframe, prototype, and UI design creation.</p></li>
-                                <li><p>Worked closely with senior designers to enhance design concepts and implement responsive design principles using HTML, CSS, and JavaScript.</p></li>                            
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
-                <div className="timeline-item">
-                    <div className="timeline-line">
-                        <div className="dot">
-                            <img src={'/svg/dot.svg'}/>
-                        </div>
-                        
-                    </div>
-
-                    <div className="timeline-item-content">
-                        <div className="date-time">
-                            <p>Jun 2022 - Jul 2022</p>
-                            <div className="location">
-                                <img src={'/svg/location.svg'}/>
-                                <p>Ahmedabad, India</p>
-                            </div>
-                        </div>
-                        <div className="item-description">
-                            <div className="item-header">
-                                <img src={"/images/meta.png"}/>
-                                <div className="item-name">
-                                    <p className="subtitle">Web Development INTERN</p>
-                                    <p className="title">BrainyBeam Technologies Pvt. Ltd.</p>
-                                </div>
-                            </div>
-                            <ul>
-                                <li><p>Conceptualized and built Product Sales Manager for vendors to integrate the inventory and product sales. It also allowed customers to see all products from various vendors in the same place. Included features like login authentication, product search, and product management.</p></li>
-                                <li><p>Built using Django, Python, HTML, CSS and Javascript.</p></li>                            
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
+export default function WorkExperience() {
+  return (
+    <section className="work-experience section-container">
+      <h2>Work Experience</h2>
+      <div className="content-container">
+        <div className="timeline-item">
+          <div className="timeline-line">
+            <div className="dot">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="misc/dot_04_l">
+                  <path
+                    id="Icon"
+                    d="M15 21.25C11.5482 21.25 8.75 18.4518 8.75 15C8.75 11.5482 11.5482 8.75 15 8.75C18.4518 8.75 21.25 11.5482 21.25 15C21.25 18.4518 18.4518 21.25 15 21.25Z"
+                  />
+                </g>
+              </svg>
             </div>
-        </section>
-    )
+            <div className="divider">
+              <div className="line" />
+            </div>
+          </div>
+          <div className="timeline-item-content">
+            <div className="date-time">
+              <div className="badge">
+                <p>Present</p>
+              </div>
+              <div className="location">
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="Icon"
+                    d="M16.0003 27.9971C14.3162 26.5606 12.7552 24.9858 11.3337 23.2891C9.20033 20.7411 6.66699 16.9465 6.66699 13.3305C6.6651 9.55387 8.93932 6.14832 12.4284 4.703C15.9175 3.25768 19.9338 4.05746 22.603 6.72914C24.3583 8.47661 25.3415 10.8537 25.3337 13.3305C25.3337 16.9465 22.8003 20.7411 20.667 23.2891C19.2454 24.9858 17.6844 26.5606 16.0003 27.9971ZM16.0003 9.33047C14.5713 9.33047 13.2508 10.0929 12.5362 11.3305C11.8217 12.5681 11.8217 14.0929 12.5362 15.3305C13.2508 16.5681 14.5713 17.3305 16.0003 17.3305C18.2095 17.3305 20.0003 15.5396 20.0003 13.3305C20.0003 11.1213 18.2095 9.33047 16.0003 9.33047Z"
+                  />
+                </svg>
+
+                <p>Vadodara, India</p>
+              </div>
+            </div>
+            <div className="item-description">
+              <div className="item-header">
+                <img src={"/images/meta.png"} />
+                <div className="item-name">
+                  <p className="subtitle">UI/UX Engineer</p>
+                  <p className="title">Electrum IT Solutions</p>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <p>
+                    Conducted user research through surveys and interviews,
+                    informing the design process.
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Produced wireframes, interactive prototypes, and high
+                    fidelity UI designs with tools like Figma, Adobe
+                    Illustrator, and Adobe After Effects, optimizing
+                    design-to-development flow.
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Collaborated with cross-functional teams to translate user
+                    requirements into effective design solutions.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="timeline-item">
+          <div className="timeline-line">
+            <div className="dot">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="misc/dot_04_l">
+                  <path
+                    id="Icon"
+                    d="M15 21.25C11.5482 21.25 8.75 18.4518 8.75 15C8.75 11.5482 11.5482 8.75 15 8.75C18.4518 8.75 21.25 11.5482 21.25 15C21.25 18.4518 18.4518 21.25 15 21.25Z"
+                  />
+                </g>
+              </svg>
+            </div>
+            <div className="divider">
+              <div className="line" />
+            </div>
+          </div>
+          <div className="timeline-item-content">
+            <div className="date-time">
+              <p>Feb 2023 - May 2023</p>
+              <div className="location">
+              <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="Icon"
+                    d="M16.0003 27.9971C14.3162 26.5606 12.7552 24.9858 11.3337 23.2891C9.20033 20.7411 6.66699 16.9465 6.66699 13.3305C6.6651 9.55387 8.93932 6.14832 12.4284 4.703C15.9175 3.25768 19.9338 4.05746 22.603 6.72914C24.3583 8.47661 25.3415 10.8537 25.3337 13.3305C25.3337 16.9465 22.8003 20.7411 20.667 23.2891C19.2454 24.9858 17.6844 26.5606 16.0003 27.9971ZM16.0003 9.33047C14.5713 9.33047 13.2508 10.0929 12.5362 11.3305C11.8217 12.5681 11.8217 14.0929 12.5362 15.3305C13.2508 16.5681 14.5713 17.3305 16.0003 17.3305C18.2095 17.3305 20.0003 15.5396 20.0003 13.3305C20.0003 11.1213 18.2095 9.33047 16.0003 9.33047Z"
+                  />
+                </svg>
+                <p>Vadodara, India</p>
+              </div>
+            </div>
+            <div className="item-description">
+              <div className="item-header">
+                <img src={"/images/meta.png"} />
+                <div className="item-name">
+                  <p className="subtitle">UI/UX ENGINEER INTERN</p>
+                  <p className="title">Electrum IT Solutions</p>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <p>
+                    During the internship, contributed to wireframe, prototype,
+                    and UI design creation.
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Worked closely with senior designers to enhance design
+                    concepts and implement responsive design principles using
+                    HTML, CSS, and JavaScript.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="timeline-item">
+          <div className="timeline-line">
+            <div className="dot">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="misc/dot_04_l">
+                  <path
+                    id="Icon"
+                    d="M15 21.25C11.5482 21.25 8.75 18.4518 8.75 15C8.75 11.5482 11.5482 8.75 15 8.75C18.4518 8.75 21.25 11.5482 21.25 15C21.25 18.4518 18.4518 21.25 15 21.25Z"
+                  />
+                </g>
+              </svg>
+            </div>
+          </div>
+
+          <div className="timeline-item-content">
+            <div className="date-time">
+              <p>Jun 2022 - Jul 2022</p>
+              <div className="location">
+              <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="Icon"
+                    d="M16.0003 27.9971C14.3162 26.5606 12.7552 24.9858 11.3337 23.2891C9.20033 20.7411 6.66699 16.9465 6.66699 13.3305C6.6651 9.55387 8.93932 6.14832 12.4284 4.703C15.9175 3.25768 19.9338 4.05746 22.603 6.72914C24.3583 8.47661 25.3415 10.8537 25.3337 13.3305C25.3337 16.9465 22.8003 20.7411 20.667 23.2891C19.2454 24.9858 17.6844 26.5606 16.0003 27.9971ZM16.0003 9.33047C14.5713 9.33047 13.2508 10.0929 12.5362 11.3305C11.8217 12.5681 11.8217 14.0929 12.5362 15.3305C13.2508 16.5681 14.5713 17.3305 16.0003 17.3305C18.2095 17.3305 20.0003 15.5396 20.0003 13.3305C20.0003 11.1213 18.2095 9.33047 16.0003 9.33047Z"
+                  />
+                </svg>
+                <p>Ahmedabad, India</p>
+              </div>
+            </div>
+            <div className="item-description">
+              <div className="item-header">
+                <img src={"/images/meta.png"} />
+                <div className="item-name">
+                  <p className="subtitle">Web Development INTERN</p>
+                  <p className="title">BrainyBeam Technologies Pvt. Ltd.</p>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <p>
+                    Conceptualized and built Product Sales Manager for vendors
+                    to integrate the inventory and product sales. It also
+                    allowed customers to see all products from various vendors
+                    in the same place. Included features like login
+                    authentication, product search, and product management.
+                  </p>
+                </li>
+                <li>
+                  <p>Built using Django, Python, HTML, CSS and Javascript.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
