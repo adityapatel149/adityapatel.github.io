cc3fed5b6b2cfbd54e2718431b179edfe704ca74
AdityaPatel149 <55062974+adityapatel149@users.noreply.github.com>
2023-10-25 07:27:42 +0000

    Implemented dark mode toggle

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "../styles.css";
 import Hero from "./Hero";
 import AboutMe from "./AboutMe";
@@ -7,10 +8,90 @@ import WorkExperience from "./WorkExperience";
 import Education from "./Education";
 import Skills from "./Skills";
 import Footer from "./Footer";
+import Switch from "react-switch";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const handleChange = () => {
+    setDarkMode((prev) => !prev);
+  };
+
+  useEffect(() => {
+    darkMode
+      ? document.documentElement.setAttribute("data-theme", "dark")
+      : document.documentElement.setAttribute("data-theme", "light");
+  }, [darkMode]);
+
   return (
     <main>
+      <label className="darkmode-switch">
+        <p>Dark Mode</p>
+        <Switch
+          onChange={handleChange}
+          checked={darkMode}
+          handleDiameter={40}
+          offColor="#9292A0"
+          onColor="#5C5C66"
+          offHandleColor="#fff"
+          onHandleColor="#090909"
+          height={48}
+          width={100}
+          borderRadius={40}
+          activeBoxShadow="0px 0px 0px 0px #fffc35"
+          uncheckedIcon={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                color: "red",
+              }}
+            >
+              <img src="/svg/moon-icon.svg" alt="" />
+            </div>
+          }
+          checkedIcon={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <img src="/svg/sun-icon.svg" alt="" />
+            </div>
+          }
+          uncheckedHandleIcon={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <img src="/svg/sun-icon.svg" alt="" />
+            </div>
+          }
+          checkedHandleIcon={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                color: "red",
+              }}
+            >
+              <img src="/svg/moon-icon.svg" alt="" />
+            </div>
+          }
+          className="react-switch"
+          id="small-radius-switch"
+        />
+      </label>
       <div className="App">
         <Hero />
         <AboutMe />
