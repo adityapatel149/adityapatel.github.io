8eebc486999ad85e0b70e9e723ac504bb4858340
AdityaPatel149 <55062974+adityapatel149@users.noreply.github.com>
2024-07-31 20:53:56 +0000

    update work experience at Groflex Technologies

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -28,7 +28,85 @@ export default function WorkExperience() {
           <div className="timeline-item-content">
             <div className="date-time">
               <div className="badge">
-                <p>Present</p>
+                <p>Dec 2023 - June 2024</p>
+              </div>
+              <div className="location">
+                <svg
+                  width="32"
+                  height="32"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    id="Icon"
+                    d="M16.0003 27.9971C14.3162 26.5606 12.7552 24.9858 11.3337 23.2891C9.20033 20.7411 6.66699 16.9465 6.66699 13.3305C6.6651 9.55387 8.93932 6.14832 12.4284 4.703C15.9175 3.25768 19.9338 4.05746 22.603 6.72914C24.3583 8.47661 25.3415 10.8537 25.3337 13.3305C25.3337 16.9465 22.8003 20.7411 20.667 23.2891C19.2454 24.9858 17.6844 26.5606 16.0003 27.9971ZM16.0003 9.33047C14.5713 9.33047 13.2508 10.0929 12.5362 11.3305C11.8217 12.5681 11.8217 14.0929 12.5362 15.3305C13.2508 16.5681 14.5713 17.3305 16.0003 17.3305C18.2095 17.3305 20.0003 15.5396 20.0003 13.3305C20.0003 11.1213 18.2095 9.33047 16.0003 9.33047Z"
+                  />
+                </svg>
+
+                <p>Vadodara, India</p>
+              </div>
+            </div>
+            <div className="item-description">
+              <div className="item-header">
+                <img src={"/images/meta.png"} />
+                <div className="item-name">
+                  <p className="subtitle">UI/UX Designer Intern</p>
+                  <p className="title">Groflex Technologies Pvt. Ltd.</p>
+                </div>
+              </div>
+              <ul>
+                <li>
+                  <p>
+                    Adapted web designs for mobile platforms to ensure
+                    responsiveness.
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Designed and developed web pages for the company website
+                    using HTML, CSS, and JavaScript.
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    Managed content with WordPress and created engaging product
+                    videos and images with Adobe After Effects, Figma and Adobe
+                    Photoshop to enhance visual appeal, user engagement, and
+                    conversions.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="timeline-item">
+          <div className="timeline-line">
+            <div className="dot">
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="misc/dot_04_l">
+                  <path
+                    id="Icon"
+                    d="M15 21.25C11.5482 21.25 8.75 18.4518 8.75 15C8.75 11.5482 11.5482 8.75 15 8.75C18.4518 8.75 21.25 11.5482 21.25 15C21.25 18.4518 18.4518 21.25 15 21.25Z"
+                  />
+                </g>
+              </svg>
+            </div>
+            <div className="divider">
+              <div className="line" />
+            </div>
+          </div>
+          <div className="timeline-item-content">
+            <div className="date-time">
+              <div className="badge">
+                <p>June 2023 - Oct 2023</p>
               </div>
               <div className="location">
                 <svg
@@ -107,7 +185,7 @@ export default function WorkExperience() {
             <div className="date-time">
               <p>Feb 2023 - May 2023</p>
               <div className="location">
-              <svg
+                <svg
                   width="32"
                   height="32"
                   viewBox="0 0 32 32"
@@ -172,7 +250,7 @@ export default function WorkExperience() {
             <div className="date-time">
               <p>Jun 2022 - Jul 2022</p>
               <div className="location">
-              <svg
+                <svg
                   width="32"
                   height="32"
                   viewBox="0 0 32 32"
